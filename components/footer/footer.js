55c83334a1e07ee46643d4949dd7456a81453afc
Brandon <50425801+brand95g@users.noreply.github.com>
2023-04-23 12:07:35 -0400

    Update footer.js
    added aria labels

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,10 +3,10 @@ import styles from './footer.module.css';
 export default function Footer() {
     return <div className={styles.Foot}>
             <ul>
-                <li className={styles.inline}><Link href="/">Home</Link></li>
-                <li className={styles.inline}><a href="https://www.linkedin.com/in/brandongasper/" target="blank">Linkedin</a></li>
-                <li className={styles.inline}><a href="https://www.instagram.com/brandon95g/" target="blank">Instagram</a></li>
-                <li className={styles.inline}><a href="https://github.com/gasperb95" target="blank">GitHub</a></li>
+                <li className={styles.inline}><Link href="/" aria-label='Link back to Home Page'>Home</Link></li>
+                <li className={styles.inline}><a href="https://www.linkedin.com/in/brandongasper/" target="blank" aria-label='Link to Linkedin Page'>Linkedin</a></li>
+                <li className={styles.inline}><a href="https://www.instagram.com/brandon95g/" target="blank" aria-label='Link to Instagram Page'>Instagram</a></li>
+                <li className={styles.inline}><a href="https://github.com/gasperb95" target="blank" aria-label='Link to my GitHub'>GitHub</a></li>
             </ul>
     </div>
   }
