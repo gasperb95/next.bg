8eb6a041c6e2440c5697730c88f6c8c8fa4f7acc
Brandon Gasper <brandon@gasper.one>
2025-05-20 16:22:41 -0400

    Setting up Blog page and rendering content

--- pages/blog/[id]/[slug].js
+++ pages/blog/[id]/[slug].js
@@ -1,6 +1,25 @@
-import { useRouter } from 'next/router'
- 
-export default function Page() {
-  const router = useRouter()
-  return <p>Post: {router.query.id}</p>
+import styles from '../../../styles/Home.module.css';
+
+export default function Page({ post }) {
+  return (
+    <div>
+      <div key={post.id}>
+        <img className={styles.feature} src={post.featuredImage} alt={post.htmlTitle} />
+        <h2>{post.htmlTitle}</h2>
+        <body dangerouslySetInnerHTML={{ __html: post.postBody }} />
+        {/* Add more post details here if needed */}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const res = await fetch(`https://api.hubspot.com/cms/v3/blogs/posts/${id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.HUBSPOT_KEY}`
+    }
+  });
+  const post = await res.json();
+  return { props: { post } };
 }
