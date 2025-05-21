0e3b249ed2229d880828550a6db31d896494fa13
Brandon Gasper <brandon@gasper.one>
2025-05-21 10:28:55 -0400

    added grid to blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,45 +7,48 @@ import Head from 'next/head';
 export default function Page({ posts }) {
   return (
     <div>
-    <Layout>
-    <div>
-        <Head>
-        <title>Brandon's Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>My Blog</h1>
-      <div className='card'>
-        {posts.map((post) => (
-          <div className={styles.card2} key={post.id}>
-            
-            <img className={styles.feature} src={post.featuredImage} alt={post.featuredImageAltText} />
-            <h2>{post.htmlTitle}</h2>
-            <p>
-              {new Date(post.created).toLocaleDateString(undefined, {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric'
-              })}
-            </p>
-            <p>{post.authorName}</p>
-            <Link href={`/blog/${post.id}/${post.slug.replace(/^\/?blog\/?/, '')}`}>
-            <p> View Post</p>
-            </Link>
+      <Layout>
+        <div>
+          <Head>
+            <title>Brandon's Blog</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <h1>My Blog</h1>
+          <div className={styles.grid}>
+            {posts.map((post) => (
+              <Link className={styles.card2}
+                key={post.id}
+                href={`/blog/${post.id}/${post.slug.replace(/^\/?blog\/?/, '')}`}
+                passHref
+              >
+                <div>
+                  <img
+                    className={styles.feature}
+                    src={post.featuredImage}
+                    alt={post.featuredImageAltText}
+                  />
+                  <h2>{post.htmlTitle}</h2>
+                  <p>
+                    {new Date(post.created).toLocaleDateString(undefined, {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })};
+                  </p>
+                  <p>{post.authorName}</p>
+                </div>
+              </Link>
+            ))}
           </div>
-        ))}
-      </div>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+          <h2>
+            <Link href="/">Back to home</Link>
+          </h2>
         </div>
-
-  
-    </Layout>
-    <footer>
-<Footer>
-</Footer>
-</footer>
-</div>
+      </Layout>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
