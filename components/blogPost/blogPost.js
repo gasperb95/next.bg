76fb9bfa024d8a23e6213fbd454588b74e945372
Brandon Gasper <brandon@gasper.one>
2025-06-18 15:23:33 -0400

    updated images

--- components/blogPost/blogPost.js
+++ components/blogPost/blogPost.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function BlogPost({ post }) {
     return (
       <div>
-        <Image width="1366" height="766"style={{ width: '100%', height: 'auto' }} className={styles.featured} src={post.data[0].featuredImage} alt={post.data[0].featuredImageAltText} />
+        <Image loading="eager" width="612" height="491" style={{ width: '100%', height: 'auto' }} className={styles.featured} src={post.data[0].featuredImage} alt={post.data[0].featuredImageAltText} />
         <h1>{post.data[0].htmlTitle}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.data[0].postBody }} />
       </div>
